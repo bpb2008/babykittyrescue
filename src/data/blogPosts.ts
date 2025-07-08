01ab46db1378e5e79b7a1a999401434f4ff9464c
bpb2008 <brittanythompson08@gmail.com>
2025-07-08 18:19:46 -0500

    fixed recent posts section

---
+++ src/data/blogPosts.ts
@@ -0,0 +1,17 @@
+export const blogPosts = [
+  {
+    title: "Neonatal Care for Kittens",
+    slug: "/resources/neonatal-care",
+    date: "August 3, 2021",
+  },
+  {
+    title: "Help Kittens Socialize",
+    slug: "/resources/socializing-kittens",
+    date: "August 3, 2021",
+  },
+  {
+    title: "Our Mission",
+    slug: "/about",
+    date: "August 3, 2021",
+  },
+];
